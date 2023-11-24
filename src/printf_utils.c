840fb765a6f17969702e381b3780c69d6c5bd759
Inf1n1ty75 <volkandu75@gmail.com>
2023-11-24 15:56:23 +0100

    added bonus

--- src/printf_utils.c
+++ src/printf_utils.c
@@ -6,11 +6,11 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 16:17:15 by vopekdas          #+#    #+#             */
-/*   Updated: 2023/11/23 14:43:53 by vopekdas         ###   ########.fr       */
+/*   Updated: 2023/11/24 15:48:35 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../include/ft_printf.h"
 
 size_t	ft_strlen(const char *s)
 {
--- src/printf_utils_bonus.c
+++ src/printf_utils_bonus.c
@@ -1,32 +1,65 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_mandatory2.c                                :+:      :+:    :+:   */
+/*   printf_utils_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/21 16:20:50 by vopekdas          #+#    #+#             */
-/*   Updated: 2023/11/24 15:48:31 by vopekdas         ###   ########.fr       */
+/*   Created: 2023/11/21 16:17:15 by vopekdas          #+#    #+#             */
+/*   Updated: 2023/11/24 15:48:32 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-int	ft_handle_c(int fmt)
+size_t	ft_strlen(const char *s)
 {
-	int	len;
+	size_t	i;
 
-	len = 0;
-	len = ft_putchar(fmt);
-	return (len);
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
 }
 
-int	ft_handle_percent(const char **fmt)
+int	ft_putchar(const char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+void	ft_putstr(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	ft_putnbr(long long nbr)
 {
 	int	len;
 
 	len = 0;
-	len = ft_putchar('%');
-		(*fmt)++;
+	if (nbr < 0)
+	{
+		len += ft_putchar('-');
+		nbr *= -1;
+	}
+	if (nbr < 10)
+	{
+		len += ft_putchar(nbr + '0');
+	}
+	else
+	{
+		len += ft_putnbr(nbr / 10);
+		len += ft_putnbr(nbr % 10);
+	}
 	return (len);
 }
